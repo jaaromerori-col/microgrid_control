678cce4461d9d5de7b6bd06c5ab9573e5bf482f2
jaaromerori-col <jaaromerori@gmail.com>
2025-10-01 20:28:13 -0700

    Add files via upload

---
+++ 2. uGrid_configured_by_code/Simulation_variable_PID_Interpolated_gain_selector.m
@@ -0,0 +1,43 @@
+tim_vec=0:1:6;                      % Used in Simulink as time vector.
+
+Ir_V=[0 100 500 1000 500 100 0];    %Used to change irradiance in simulink
+I_pv_sp_V=0.0662*Ir_V+0.0549;       %Used to change current set-point in simulink
+RL_var_vec=[1 6 8 15 7 3 5];       %Used to change load resistance in simulink
+S1=sim('Variable_PID_Interpolated_gains.slx');   %Run the simulation
+Data_graph=squeeze(S1.graph.Data);  %Obtain data from the simulation
+V_bus=Data_graph(:,1);              %Voltage vector
+I_pv=Data_graph(:,2);               %Current of the solar array
+I_pv_ref=Data_graph(:,3);           %Current set point
+V_sup_limit=Data_graph(:,4);        %5 % limit for the voltage
+V_inf_limit=Data_graph(:,5);        %5 % limit for the voltage
+time=[0:1e-6:0.08]*1000;            % timein ms
+%% Code to generate a nice graph
+figure('Units','inches','Position',[0 0 3.5 2.2],'PaperPositionMode','auto')
+yyaxis left
+plot(time,V_bus)
+axis([0 time(end) 0 90])
+set(gca,'Units','normalized','YTick',0:10:90,'XTick',0:10:80,'Position',[.15 .2 .75 .7]...
+    ,'FontUnits','points','FontWeight','normal','FontSize',9,'FontName','SansSerif')
+ylabel({'Voltage(V)'},'FontUnits','points','interpreter','latex','FontSize',9,'FontName','Times')
+xlabel('Time(ms)','FontUnits','points','FontWeight','normal','FontSize',9,'FontName','Times')
+
+yyaxis right
+plot(time,I_pv)
+legend({'$V_L$','$I_{pv}$'},'FontWeight','normal','interpreter','latex','FontSize',7,...
+    'FontName','Times','Location','Northeast') 
+axis([0 time(end) 0 70])
+set(gca,'Units','normalized','YTick',0:10:70,'XTick',0:10:80,'Position',[.15 .2 .75 .7]...
+    ,'FontUnits','points','FontWeight','normal','FontSize',9,'FontName','SansSerif')
+ylabel({'Current(A)'},'FontUnits','points','interpreter','latex','FontSize',9,'FontName','Times')
+
+hold
+yyaxis left
+p3=plot(time,V_sup_limit,'--')
+p3.Annotation.LegendInformation.IconDisplayStyle = 'off';
+p4=plot(time,V_inf_limit,'--')
+p4.Annotation.LegendInformation.IconDisplayStyle = 'off';
+
+yyaxis right
+p5=plot(time,I_pv_ref,'--')
+p5.Annotation.LegendInformation.IconDisplayStyle = 'off';
+grid on
---
+++ 2. uGrid_configured_by_code/readme.txt
@@ -0,0 +1,9 @@
+Microgrid simulation changed by code.
+
+This folder contains .m and .slx files that work together.
+
+1. Add all files to the Matlab path.
+2. There is a .m and .slx file for each control strategy. Open the .m file you want to work with. Configure the vectors Ir_V, I_pv_sp_V and RL_var_vec to change the parameters in the simulation.
+3. Run the .m code
+
+You will see the response of the microgrid under the configured conditions. The .m file was designed to manage the simulink simulation from the code.
